65aeb723cc9dc8a75f1429e7aa9237260303e6af
Halyna Marchenko <marchenkohalyna888@gmail.com>
2025-01-20 23:03:38 +0100

    upgrading header background

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import { HeaderMain } from "./Header.styled"
+
+const Header = () => {
+  return (
+    <HeaderMain>
+      <div className="ellipse1"></div>
+      <div className="ellipse2"></div>
+    </HeaderMain>
+  );
+}
+
+export default Header
---
+++ src/components/Header/Header.styled.jsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const HeaderMain = styled.header`
+  background: linear-gradient(
+    266.47deg,
+    #2713a1 -15.35%,
+    #a11392 24.88%,
+    #efac59 101.06%
+  );
+  border-radius: 0 0 40px 40px;
+  width: 100%;
+  height: 400px;
+  position: relative;
+
+  .ellipse1 {
+    background: linear-gradient(
+      229.54deg,
+      hsla(0, 0%, 100%, 0.15) 15.93%,
+      hsla(0, 0%, 100%, 0.2) 71.29%
+    );
+    border-radius: 50%;
+    height: 285px;
+    width: 285px;
+    position: absolute;
+    left: -102px;
+    top: 184px;
+  }
+
+  .ellipse2 {
+    background: linear-gradient(
+      140.63deg,
+      hsla(0, 0%, 100%, 0.15) 13.32%,
+      hsla(0, 0%, 100%, 0.2) 69.77%
+    );
+    border-radius: 50%;
+    height: 332px;
+    width: 332px;
+    position: absolute;
+    right: -172px;
+    top: -125px;
+  }
+`;
