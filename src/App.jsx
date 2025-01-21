471c88c9993fb86f97f5b3a7f0ca631aa8cbcafc
Halyna Marchenko <marchenkohalyna888@gmail.com>
2025-01-21 20:11:24 +0100

    integrating colors and fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-
 import './App.css'
 import Header from './components/Header/Header'
+import  Container  from './components/Common/Container/Container'
 
 function App() {
   
 
   return (
     <>
-     <Header />
+      <Header />
+      <main>
+        <Container>
+
+        </Container>
+      </main>
     </>
   )
 }
