5a5f7ba380ae7265cf6dc72071f3afa2239446ec
Halyna Marchenko <marchenkohalyna888@gmail.com>
2025-03-04 21:29:03 +0100

    set up Postcard, PostList breakpoints

--- src/components/Hero/PostsList/PostCard/PostCard.styled.jsx
+++ src/components/Hero/PostsList/PostCard/PostCard.styled.jsx
@@ -13,49 +13,65 @@ export const PostCardContainer = styled.div`
     transform: scale(1.05);
     box-shadow: 0 8px 12px rgba(0, 0, 0, 0.2);
   }
+
+  @media screen and (min-width: 768px) {
+    width: 90%;
+  }
+
+  @media screen and (min-width: 1280px) {
+    width: 100%;
+  }
 `;
 
 export const PostImage = styled.img`
   width: 100%;
-  height: 100%;
+  height: auto;
   border-radius: 10px;
 `;
 
 export const PostVideo = styled.video`
   width: 100%;
-  height: 100%;
+  height: auto;
   border-radius: 10px;
 `;
 
 export const InfoContainer = styled.div`
   display: flex;
   flex-direction: column;
-  padding: 10px;
+  padding: 8px;
+  border-radius: 10px;
+  width: 100%;
+  gap: 8px;
+  background-color: var(--bg-main-darker);
+  color: var(--main-text);
+  font-size: 16px;
+
+  @media screen and (min-width: 768px) {
+    padding: 10px;
+    font-size: 14px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    font-size: 18px;    
+  }
+`;
+
+export const PostMeta = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+  padding: 6px;
   border-radius: 10px;
   width: 100%;
   gap: 10px;
   background-color: var(--bg-main-darker);
   color: var(--main-text);
-  font-size: 18px;
-`;
-
-export const PostMeta = styled.div`
-  display: flex;
-  flex-direction: flex;
-  align-items: flex-start;
-  justify-content: flex-start;
-  padding: 8px;
-  border-radius: 10px;
-  width: 100%;
-  gap: 15px;
-  background-color: var(--bg-main-darker);
-  color: var(--main-text);
-  font-size: 18px;
+  font-size: 16px;
 
   p {
     display: flex;
     align-items: center;
-    gap: 6px;
+    gap: 4px;
   }
 
   svg {
@@ -65,26 +81,42 @@ export const PostMeta = styled.div`
 
   span {
     display: flex;
-    gap: 6px;
+    gap: 4px;
     align-items: center;
     margin-left: auto;
+  }
+
+  @media screen and (min-width: 768px) {
+    padding: 8px;
+    gap: 15px;
+    font-size: 14px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    font-size: 18px;
   }
 `;
 
 export const TagsContainer = styled.div`
   position: absolute;
-  right: 10px;
-  top: 10px;
+  right: 5px;
+  top: 5px;
   display: inline-flex;
   align-items: center;
   background: hsla(0, 0%, 9%, 0.6);
   border-radius: 44px;
-  padding: 4px 10px;
-  gap: 8px;
+  padding: 2px 8px;
+  gap: 6px;
   justify-content: center;
   list-style: none;
-`;
 
+  @media screen and (min-width: 768px) {
+    right: 10px;
+    top: 10px;
+    padding: 4px 10px;
+    gap: 8px;
+  }
+`;
 
 export const TagsButton = styled.button`
   background: transparent;
--- src/components/Hero/PostsList/PostsList.styled.jsx
+++ src/components/Hero/PostsList/PostsList.styled.jsx
@@ -2,7 +2,17 @@ import styled from "styled-components";
 
 export const PostsContainer = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: 1fr;
   justify-items: center;
-  gap: 20px;
+  gap: 10px;
+
+  @media screen and (min-width: 768px) {
+    grid-template-columns: repeat(2, 1fr);
+    gap: 15px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    grid-template-columns: repeat(3, 1fr);
+    gap: 20px;
+  }
 `;
