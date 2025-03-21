b95f0142f2e305b7835a74bd24df8e888d8001db
Halyna Marchenko <marchenkohalyna888@gmail.com>
2025-03-21 22:30:57 +0100

    updating tabs

--- src/components/Hero/Tabs/Tabs.jsx
+++ src/components/Hero/Tabs/Tabs.jsx
@@ -1,15 +1,25 @@
 import { TabsContainer, TabLink } from "./Tabs.styled"
-
+import { useLocation } from "react-router-dom";
 
 const Tabs = () => {
+  const location = useLocation(); 
+  const currentPath = location.pathname.split("/").pop(); 
+
   return (
     <>
       <TabsContainer>
-        <TabLink to="posts">Posts</TabLink>
+        <TabLink
+          to="posts"
+          className={
+            currentPath === "" || currentPath === "posts" ? "active" : ""
+          }
+        >
+          Posts
+        </TabLink>
         <TabLink to="stories">Stories</TabLink>
         <TabLink to="highlights">Highlights</TabLink>
         <TabLink to="reels">Reels</TabLink>
-          </TabsContainer>
+      </TabsContainer>
     </>
   );
 }
